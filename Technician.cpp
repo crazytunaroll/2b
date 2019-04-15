7f7deb3da0f9f5130d75d5371c8babf526750921
ace184 <42984402+ace184@users.noreply.github.com>
2019-04-15 13:34:32 -0400

    Update Technician.cpp

--- Technician.cpp
+++ Technician.cpp
@@ -6,27 +6,60 @@ using namespace std;
 
 Technician::Technician(){
 
+
+first name = "";
+last name = "";
 ID=0;
 level=0;
 
 };
 
+
+Technician::setName(){
+	
+	cout << "What is the technician's first name? \n" << endl;
+	cin >> firstname;
+	
+	cout << "What is the tehnician's last name? \n" << endl;
+	cin >> lastname;
+	
+}
+
+Technician::getName(){
+	
+	string firstname;
+	string lastname;
+	
+	firstname = first name;
+	lastname = last name;
+
+	
+}
+
 Technician::setID(){
+	
+	cout << "What is the technician's ID level? \n" << endl;
+	cin >> ID;
 
 
 };
 
 Technician::getID(){
-
+	
+	int ID;
 
 };
 
 Technician::setLvl(){
+	
+	cout << "What is the technician's level? \n";
+	cin >> Level;
 
 
 };
 
 Technician::getLvl(){
 
+	int level;
 
 };
