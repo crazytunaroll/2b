acb59f68a2c427408a1307391077a1f68448e490
tsoutting <48999803+tsoutting@users.noreply.github.com>
2019-04-20 18:50:15 -0400

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -9,16 +9,16 @@ class Customer:public Person{
 
 public:
 		Customer();
-		void setEquip(int);
-    void setID(string);
-    virtual void setName(string fname,string lname);
+	//	void setEquip(int);
+    	 string setCustID();
+    //virtual void setName(string fname,string lname);
 		virtual string getName();
 		int getEquip();
 		string getID();
 private:
 	
 		int equipment;
-		string ID;
+		string CustID;
 
 
 };
