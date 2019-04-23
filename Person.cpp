aa52a87e8d0de2e8e3491fc9a651262838299206
Nate2353 <48999945+Nate2353@users.noreply.github.com>
2019-04-23 14:29:09 -0400

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -4,29 +4,15 @@
 using namespace std;
 
 Person::Person(){
+  fname="";
+  lname="";
+}
 
+void Person::setName(string firstn, string lastn){
+ fname = firstn;
+ lname = lastn;
+}
+string getName(){
+  return 0;
+}
 
-fname="";
-lname="";
-
-};
-
-Person::setName(){
-
- cout<<"Enter first name"<<endl;
- cin>>fname;
- cout<<"Enter last name"<<endl;
- cin>>lname;
-};
-
-Person::getName(string,string){
-
-string firstn;
-string lastn;
-string fullname;
-
-firstn=fname;
-lastn=lname;
-
-fullname = fname + " " +lname;
-};
