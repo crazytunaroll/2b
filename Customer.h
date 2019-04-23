f9ac60b784a6a5d499331b6ce3013c10a1d22568
Nate2353 <48999945+Nate2353@users.noreply.github.com>
2019-04-23 14:27:16 -0400

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -5,20 +5,22 @@ using namespace std;
 
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
-class Customer:public Person{
+class Customer: public Person{
 
 public:
 		Customer();
-	//	void setEquip(int);
-    	 string setCustID();
-    //virtual void setName(string fname,string lname);
+		void setEquip(int equipID);
+    void setCustID(int id);
+    void setName(string fname,string lname);
 		virtual string getName();
 		int getEquip();
 		string getID();
 private:
 	
 		int equipment;
-		string CustID;
+		int ID;
+    string fname;
+    string lname;
 
 
 };
