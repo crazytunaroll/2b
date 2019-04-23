eaa304cca8405a29a90632674f37645cfc7c15f9
Nate2353 <48999945+Nate2353@users.noreply.github.com>
2019-04-23 14:28:22 -0400

    Update Technician.h

--- Technician.h
+++ Technician.h
@@ -1,5 +1,6 @@
 //Techician.h
 #include <iostream>
+#include"Person.h"
 using namespace std;
 
 #ifndef TECHNICIAN_H
@@ -8,14 +9,17 @@ class Technician:public Person{
 
 public:
 		Technician();
-		setID();
-		getID();
-		setLvl();
-		getLvl();
+    void setName(string fname,string lname);
+		virtual string getName() const;
+		void setID(int techID);
+		int getID();
+		void setLvl(int Level);
+		int getLvl();
 	
 private:
 		int ID;
 		int level;
-
+    string fname;
+    string lname;
 };
 #endif
